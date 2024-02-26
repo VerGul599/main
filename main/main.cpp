dc967e5e539a0b47a121db685c0e7d6c7669e68d
Вероника <@WIN-BBNSI5FE1UN>
2024-02-26 21:40:18 +0500

    Добавьте файлы проекта.

---
+++ main/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+#include <Windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double distance, time;
+
+    std::cout << "Введите расстояние до аэропорта (в километрах): ";
+    std::cin >> distance;
+
+    std::cout << "Введите время, за которое нужно доехать (в часах): ";
+    std::cin >> time;
+
+    double speed = distance / time;
+
+    std::cout << "Чтобы доехать до аэропорта вовремя, Вам нужно ехать со скоростью " << speed << " км/ч." << std::endl;
+
+    return 0;
+}
